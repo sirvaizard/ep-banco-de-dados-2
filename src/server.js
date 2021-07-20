a7eb5e9cffc892d1c1fa2eef3362bfece14a972f
Victor Lopes <thevictorfl@gmail.com>
2021-07-19 23:09:03 -0300

    get port from env variable

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 import server from './app.js'
 
-server.listen(3000, () => console.log('listening at 3000'))
+const port = process.env.PORT || 3000
+
+server.listen(3000, () => console.log(`listening at ${port}`))
