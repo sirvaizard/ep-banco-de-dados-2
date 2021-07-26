192d82023000b50e0baacecffbcb3e2eb968b471
Victor Lopes <thevictorfl@gmail.com>
2021-07-25 21:56:21 -0300

    feat: add endpoint to show single hospedagem

--- src/app/controllers/HospedagemController.js
+++ src/app/controllers/HospedagemController.js
@@ -13,6 +13,24 @@ class HospedagemController {
 
         return res.status(200).json(hospedagens)
     }
+
+    async show (req, res) {
+        const { id } = req.params
+
+        const hospedagem = await db.query(sql`
+            SELECT *
+            FROM HOSPEDAGEM
+            WHERE IDHOSPEDAGEM = ${id}
+        `)
+
+        if (!hospedagem) {
+            return res.status(400).json({
+                error: 'Hospedagem não existe.'
+            })
+        }
+
+        return res.status(200).json(hospedagem)
+    } 
 }
 
 export default new HospedagemController()
