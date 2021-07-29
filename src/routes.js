56b930dc2808d72f08953c4d3c365bccf4b9caa0
Victor Lopes <thevictorfl@gmail.com>
2021-07-29 14:46:10 -0300

    feat: add hospede routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import CheckoutController from './app/controllers/CheckoutController.js'
 import HospedagemController from './app/controllers/HospedagemController.js'
 import HospedesHospedagemController from './app/controllers/HospedesHospedagemController.js'
+import HospedeController from './app/controllers/HospedeController.js'
 
 const routes = new Router()
 
@@ -11,5 +12,7 @@ routes.get('/checkout/:id', CheckoutController.show)
 routes.get('/hospedagens', HospedagemController.index)
 routes.get('/hospedagens/:id', HospedagemController.show)
 routes.get('/hospedagens/:id/hospedes', HospedesHospedagemController.show)
+routes.get('/hospedes', HospedeController.index)
+routes.get('/hospedes/:id', HospedeController.show)
 
 export default routes
